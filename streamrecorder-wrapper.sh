41371cc213cdcef58414a733583fafaf382c44c0
Gordon Parikh <gparikh@srfconsulting.com>
2021-11-12 14:07:38 -0600

    Changed to use CamelCase path

--- streamrecorder-wrapper.sh
+++ streamrecorder-wrapper.sh
@@ -2,7 +2,8 @@
 
 export PYTHONPATH=$HOME/python-packages:$PYTHONPATH
 
-STREAMRECORDER_PATH=$HOME/streamrecorder
+STREAMRECORDER_PATH=$HOME/StreamRecorder
 
 cd $STREAMRECORDER_PATH
 $STREAMRECORDER_PATH/streamrecorder.py
+
